9ecc4435c3dda1103e99dd68787ac47ea9a285d8
asoa1 <127538753+asoa1@users.noreply.github.com>
2025-06-15 12:48:04 +0100

    Create hug.js

---
+++ commands/hug.js
@@ -0,0 +1,15 @@
+export const command = 'hug';
+
+export async function execute(sock, msg) {
+  const hugStickerPath = './media/hug.webp'; // Make sure this file exists
+
+  if (!fs.existsSync(hugStickerPath)) {
+    await sock.sendMessage(msg.key.remoteJid, { text: '❌ Hug sticker not found.' }, { quoted: msg });
+    return;
+  }
+
+  const buffer = fs.readFileSync(hugStickerPath);
+  await sock.sendMessage(msg.key.remoteJid, {
+    sticker: buffer
+  }, { quoted: msg });
+}
